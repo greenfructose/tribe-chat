72b45d8d085a87efb35aa90583bb4fc5a2c8b30d
greenfructose <justinclarkturney@gmail.com>
2022-05-09 15:48:07 -0500

    Working on auth with firebase, added loading

---
+++ components/Loading.js
@@ -0,0 +1,24 @@
+import { Circle } from 'better-react-spinkit';
+
+function Loading() {
+  return (
+    <center
+      style={{ display: 'grid', placeItems: 'center', height: '100vh'}}
+    >
+      <div>
+        <img
+          src='favicon.ico'
+          alt='Logo'
+          style={{ marginBottom: 10 }}
+          height={200}
+        />
+        <Circle
+          color='#3CBC28'
+          size={60}
+        />
+      </div>
+    </center>
+  )
+}
+
+export default Loading
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,8 +3,20 @@ import styled from 'styled-components';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchIcon from '@material-ui/icons/Search';
+import * as EmailValidator from 'email-validator';
 
 function Sidebar() {
+  const createChat = () => {
+    const input = prompt(
+      'Please enter an email address for the user you wish to chat with'
+    );
+
+    if (!input) return null;
+
+    if (EmailValidator.validate(input)) {
+      
+    }
+  }  
   return (
     <Container>
       <Header>
@@ -22,7 +34,7 @@ function Sidebar() {
         <SearchIcon />
         <SearchInput placeholder='Search in chats'/>
       </Search>
-      <SidebarButton>
+      <SidebarButton onClick={createChat}>
         Start a new chat
       </SidebarButton>
     </Container>
