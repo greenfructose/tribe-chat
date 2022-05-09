279ec3dae878df054cd26b87377bec423eb2df67
greenfructose <justinclarkturney@gmail.com>
2022-05-09 15:58:48 -0500

    Working on Auth

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,21 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../firebase';
 import Login from './login';
 import Loading from '../components/Loading';
+import firebase from 'firebase';
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      db.collection('users').doc(user.uid).set({
+        email: user.email,
+        lastSeen: firebase.firestore.FieldValue.serverTimeStamp(),
+        photoURL: user.photoURL 
+      }, { merge: true });
+    }
+  }, [user])
+  
 
   if(loading) return <Loading />
 
