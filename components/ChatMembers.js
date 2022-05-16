2c05ba46415966db77c38a81ad901afc1fc0c06a
greenfructose <justinclarkturney@gmail.com>
2022-05-16 13:34:08 -0500

    Changed registerd members vs unregistered styling

--- components/ChatMembers.js
+++ components/ChatMembers.js
@@ -16,22 +16,33 @@ import {
 import {
   db
 } from '../firebase';
+import { Loading } from '../components/Loading'
 
-function ChatMembers(emails, users, id) {
+function ChatMembers(emails) {
+  const [registeredMembers, setRegisteredMembers] = useState([]);
+  const [unregisteredMembers, setUnregisteredMembers] = useState([]);
+  useEffect(() => async () => {
+    await getRegisteredMembers(emails.emails).then((result) => {
+      if (result) {
+        setRegisteredMembers(result)
+      }
+    })
+  }, [emails])
+  useEffect(() => async () => {
+    await getUnregisteredMembers(emails.emails).then((result) => {
+      if (result) {
+        setUnregisteredMembers(result)
+      }
+    })
+  }, [emails])
 
 
-
+  console.log(`Registered members: ${registeredMembers}`)
   return (
-
     <MembersContainer>
-      {emails.emails.map((email) => 
-
-          <RegisteredMember key={email}>{email}</RegisteredMember>
-
-          // <UnregisteredMember>{email}</UnregisteredMember>
-  
-      )}
-      
+      <RegisteredMember>{registeredMembers.join(', ')}</RegisteredMember>
+      {registeredMembers.length>0 && unregisteredMembers.length>0 && <Breaker>|</Breaker>}
+      <UnregisteredMember>{unregisteredMembers.join(', ')}</UnregisteredMember>
     </MembersContainer>
   )
 }
@@ -49,7 +60,10 @@ const UnregisteredMember = styled.p`
 margin-right: 0.5rem;
 color: red;
 `;
-
+const Breaker = styled.p`
+margin-left: 0;
+margin-right: 0.5rem;
+`;
 
 const isRegistered = async (member) => {
   let result = false;
@@ -63,4 +77,47 @@ const isRegistered = async (member) => {
     })
   });
   return result;
-}
+};
+
+const getRegisteredMembers = async (emails) => {
+  let reg = [];
+  for (let email of emails) {
+    console.log(`Checking if ${email} is registered...`)
+    await isRegistered(email).then((result) => {
+      if (result) {
+        reg.push(email)
+        console.log(`${email} is registered`)
+      }
+    });
+  };
+  console.log(`Reg: ${reg}`)
+  return reg;
+};
+
+const getUnregisteredMembers = async (emails) => {
+  let unreg = [];
+  for (let email of emails) {
+    console.log(`Checking if ${email} is registered...`)
+    await isRegistered(email).then((result) => {
+      if (!result) {
+        unreg.push(email)
+        console.log(`${email} is registered`)
+      }
+    });
+  };
+  console.log(`Reg: ${unreg}`)
+  return unreg;
+};
+
+// const getUnregisteredMembers = async (emails) => {
+//   let unregisteredMembers = [];
+//   emails.forEach(email => {
+//     isRegistered(email).then((result) => {
+//       if (!result) {
+//         console.log(`${email} is not registered`)
+//         unregisteredMembers.push(email);
+//       }
+//     })
+//   });
+//   return unregisteredMembers;
+// };
